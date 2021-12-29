076aaacf362456791680432ba8373ace79e0b0d9
osagie99 <surgemacbookpro@Osagies-MacBook-Pro.local>
2021-12-29 15:51:04 +0100

    First Commit

---
+++ src/components/cardComponent/CardComponent.jsx
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+import { Container, Row, Col, Form, Button, InputGroup } from 'react-bootstrap'
+import logo from "../../img/LOGO.png";
+import './cardStyles.css'
+
+const CardComponent = () => {
+  return (
+    <div className="container">
+      <div className="column1">
+        <h1>Sponsor Now</h1>
+        <p>
+          Join us to spread God's
+          <span>message of Love to His people</span>
+          <span>by sponsoring copies of</span>
+          Now That You Are Born Again!
+        </p>
+      </div>
+      <div className="column2">
+        <img src={logo} alt="" />
+        <form action="">
+          <input type="text" placeholder="Full Name" id="fname" name="fname" />
+          <br />
+          <input type="email" placeholder="Email" id="email" name="email" />
+          <br />
+          <input type="text" placeholder="Amount" id="amount" name="amount" />
+          <br />
+          <input
+            type="text"
+            placeholder="Currency"
+            id="currency"
+            name="currency"
+          />
+          <br />
+          <button type="submit">Donate Now</button>
+        </form>
+        <p>© 2021 Loveworld Publishing Limited. All Rights Reserved.</p>
+      </div>
+    </div>
+  );
+}
+
+export default CardComponent
